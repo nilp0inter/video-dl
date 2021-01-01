238c8827989dd4e70a34f312abcdeef97b3e432b
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2021-01-01 21:02:08 +0100

    feat: main model and interactions
    Co-authored-by: Leticia Garcia <leti.garcia.martin@gmail.com>

--- spa/src/Example.elm
+++ spa/src/Example.elm
@@ -9,22 +9,79 @@ import Bulma.Elements exposing (..)
 import Bulma.Components exposing (..)
 import Bulma.Columns as Columns exposing (..)
 import Bulma.Layout exposing (..)
+import Maybe.Extra exposing (orElse)
 
-import Html exposing ( Html, Attribute, main_, span, a, p, img ,br, text, strong, option, small, input, i)
+import Html exposing ( Html, Attribute, main_, span, a, p, img ,br, text, strong, option, small, input, i, code)
 import Html.Events exposing ( onInput, onClick )
 import Html.Attributes exposing ( attribute, style, src, placeholder, type_, href, rel, class, value)
 
-type alias Model = {
-        url : String
-        }
+type alias Model = 
+  { url : String
+  , videos : List Video 
+  , showingModal : Bool
+  , videoInModal : Maybe Video
+  , textInModal : Maybe String
+  }
+
+type VideoStatus = Done
+                 | Submitted
+                 | Error
+                 | Pending
+
+type alias Video = 
+  { id : String
+  , url : String
+  , title : Maybe String
+  , status : VideoStatus
+  , progress: Maybe Float
+  }
 
 type Msg = UpdateURL String
          | DownloadVideo
+         | DeleteVideo Video
+         | ViewStdOut Video
+         | ViewStdErr Video
+         | CloseModal
+
+myVideos = 
+  [ { id = "id1"
+    , url = "www.miurl.es"
+    , title = Just "Video1"
+    , status = Pending
+    , progress = Just 0.2
+    }
+  , { id = "id2"
+    , url = "www.miurl2.es"
+    , title = Just "Video2"
+    , status = Error
+    , progress = Nothing
+    }
+  , { id = "id3"
+    , url = "www.miurl3.es"
+    , title = Just "Video3"
+    , status = Done
+    , progress = Just 1
+    }
+  , { id = "id4"
+    , url = "www.miurl4.es"
+    , title = Nothing
+    , status = Submitted
+    , progress = Nothing
+    }
+  ]
+
+statusToColor: VideoStatus -> Color
+statusToColor videostatus
+  = case videostatus of
+          Done -> Success
+          Submitted -> Warning
+          Error -> Danger
+          Pending -> Info
 
 main : Program () Model Msg
 main
   = Browser.sandbox
-    { init = {url=""}
+    { init = {url="", videos=myVideos, showingModal=False, videoInModal=Nothing, textInModal=Just "Logging"}
     , view = view
     , update = update
     }
@@ -41,17 +98,39 @@ update msg model
   = case msg of
       UpdateURL u -> { model | url = u }
       DownloadVideo -> { model | url = "" }
+      DeleteVideo video -> { model | url = "deleted video" }
+      ViewStdOut video -> { model | showingModal = True, videoInModal = Just video }
+      ViewStdErr video -> { model | showingModal = True, videoInModal = Just video }
+      CloseModal -> { model | showingModal = False, videoInModal = Nothing }
 
 view : Model -> Html Msg
 view model
   = main_ []
     [ stylesheet
     , fontAwesomeCDN
+    , videoModal model
     , videoHeader model
-    , videoList
+    , videoList model
     , videoFooter
     ]
 
+
+videoModal : Model -> Html Msg
+videoModal model
+  = modal model.showingModal []
+    [ modalBackground [] []
+    , modalCard [] [ modalCardHead [] [ modalCardTitle [] [ text (model.videoInModal
+                                                                  |> Maybe.andThen .title
+                                                                  |> orElse (model.videoInModal |> Maybe.map .url)
+                                                                  |> Maybe.withDefault "N/A") ] ]
+                   , modalCardBody [] [ model.textInModal
+                                        |> Maybe.map (\t -> code [ style "display" "flex" ] [ text t ])
+                                        |> Maybe.withDefault (icon Standard [] [ i [ class "fa fa-spinner fa-pulse" ] [] ])
+                                      ]
+                   , modalCardFoot [] [ ]
+                   ]
+    , modalClose Large [ onClick CloseModal ] []
+    ]
 
 columnSizes = { mobile = Just Auto
               , tablet = Just Width9
@@ -81,20 +160,23 @@ videoHeader model
       ]
     ]
 
-videoList : Html Msg
-videoList
+videoToRow : Video -> Html Msg
+videoToRow video
+  = tableRow False [] [ tableCell [] [ text (Maybe.withDefault video.url video.title) ]
+                                     , tableCell [] [ easyProgress { progressModifiers | color = statusToColor video.status } [] (Maybe.withDefault 1 video.progress) ]
+                                     , tableCell [] [ a [ onClick (ViewStdOut video) ] [ icon Standard [] [ i [ class "fa fa-file-text-o" ] [] ] ] 
+                                                    , a [ onClick (ViewStdErr video) ] [ icon Standard [] [ i [ class "fa fa-file-text" ] [] ] ]
+                                                    , a [ onClick (DeleteVideo video) ] [ icon Standard [] [ i [ class "fa fa-trash" ] [] ] ]
+                                                    ]
+                                     ]
+
+videoList : Model -> Html Msg
+videoList model
   = let
       headerCells = [ tableCellHead [ style "width" "60%" ] [ text "Video" ]
                     , tableCellHead [] [ text "Status" ]
                     , tableCellHead [ style "width" "10%" ] [ text "Actions" ]
                     ]
-      tableEntry = tableRow False [] [ tableCell [] [ text "https://www.youtube.com/watch?v=SGBP3sG3a9Y" ]
-                                     , tableCell [] [ easyProgress { progressModifiers | color = Success } [] 50.0 ]
-                                     , tableCell [] [ icon Standard [] [ i [ class "fa fa-file-text-o" ] [] ]
-                                                    , icon Standard [] [ i [ class "fa fa-file-text" ] [] ]
-                                                    , icon Standard [] [ i [ class "fa fa-trash" ] [] ]
-                                                    ]
-                                     ]
 
     in
         section NotSpaced [] [
@@ -105,9 +187,7 @@ videoList
                                            , hoverable = True } []
                     [ tableHead [] [ tableRow False [] headerCells
                                    ]
-                    , tableBody [] [ tableEntry
-                                   , tableEntry
-                                   , tableEntry ]
+                    , tableBody [] (List.map videoToRow model.videos)
                     , tableFoot [] [ tableRow False [] headerCells
                                    ]
                     ]
@@ -125,7 +205,7 @@ videoFooter
           , text " a tacky web ui for "
           , a [ href "https://youtube-dl.org" ] [ text "youtube-dl" ]
           , text " by "
-          , a [ href "https://github.com/" ] [ text "Leticia García Martín" ]
+          , a [ href "https://github.com/laetitiae" ] [ text "Leticia García Martín" ]
           , text " and "
           , a [ href "https://github.com/nilp0inter" ] [ text "Roberto Abdelkader Martínez Pérez" ]
           , text ". The source code is licensed "
